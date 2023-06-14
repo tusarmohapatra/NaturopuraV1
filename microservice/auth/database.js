cfb1c6bdc85cd59b80fa4301ec5113b08187bccc
dwitidibyajyoti <dwitidibyajyoti@gmail.com>
2023-06-14 18:24:13 +0530

    project setup

--- microservice/auth/database.js
+++ microservice/auth/database.js
@@ -1,10 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname', {
-  dialect: 'postgres',
-  logging: false,
+const sequelize = new Sequelize('app_db', 'root', 'root', {
+  host: 'mysql', // Replace with the host of your Docker MySQL container
+  dialect: 'mysql',
 });
-
 sequelize
   .authenticate()
   .then(() => console.log('Connection has been established successfully.'))
